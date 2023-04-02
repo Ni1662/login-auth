acd534b31ac93259a97bfc9bb64a107baad4d9eb
Nitesh <niteshkumar.nk649@gmail.com>
2023-04-02 12:41:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import { Container, Col, Row } from "react-bootstrap";
+import Account from "./Account";
+import FreeComponent from "./FreeComponent";
+import ProtectedRoutes from "./ProtectedRoutes";
+import AuthComponent from "./AuthComponent";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <h1>React Authentication Tutorial</h1>
+
+          <section id="navigation">
+            <a href="/">Home</a>
+            <a href="/free">Free Component</a>
+            <a href="/auth">Auth Component</a>
+          </section>
+        </Col>
+      </Row>
+
+      <Switch>
+        <Route exact path="/" component={Account} />
+        <Route exact path="/free" component={FreeComponent} />
+        <ProtectedRoutes path="/auth" component={AuthComponent} />
+      </Switch>
+      
+    </Container>
+
+
   );
 }
 
